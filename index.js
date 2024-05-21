69d8c4bb1e4d25beaadbccfe9fe35b7c51298c17
Rian <142219160+Rianofc@users.noreply.github.com>
2024-05-21 14:27:10 +0800

    index.js

--- index.js
+++ index.js
@@ -298,7 +298,7 @@ app.get('/api/bingimg', async (req, res) => {
             if (data.length > 0) {
       for (let i = 0; i < data.length; i++) {
           if (!data[i].endsWith(".svg")) {
-		  const result = data[0]
+		  const result = data[i]
     res.status(200).json({
       status: 200,
       creator: "RIAN X EXONITY",
